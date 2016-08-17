d93645dd44b2de0c7ce6da7eb37bce9253521d66
Alvin Kato J.R <alvinkatojr@gmail.com>
2016-08-17 05:39:51 +0300

    Adds questions

---
+++ challenge-1/29-flatten-array.rb
@@ -0,0 +1,1 @@
+# Write a program that will take a nested list and returns a single list with all values
---
+++ challenge-1/30-reverse.rb
@@ -0,0 +1,2 @@
+# Write a function to reverse a list, using the least possible iterations
+# and the function should have a space complexity of O(1).

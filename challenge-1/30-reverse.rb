88a5fad75f78c2832eb2e497269051639d8e7fc9
Alvin Kato J.R <alvinkatojr@gmail.com>
2016-08-23 21:05:29 +0300

    Adds 2 different reverse problems

--- challenge-1/30-reverse.rb
+++ challenge-1/30-reverse.rb
@@ -1,2 +1,7 @@
-# Write a function to reverse a list, using the least possible iterations
+# Write a function to reverse a linked list, using the least possible iterations
 # and the function should have a space complexity of O(1).
+# [10, 20, 8] => [8, 20, 10]
+# [10, 20, 8, 7, 9] => [9, 7, 8, 20, 10, ]
+
+def reverse(array)
+end
